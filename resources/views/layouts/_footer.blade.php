83b00c5739eb337f0f0618948d74e11b82a5a6e8
Boris <chenjunchi414@gmail.com>
2021-02-21 08:49:08 +0000

    basic frame

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            Designed by <a href="https://leo108.com" target="_blank">Boris</a><span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">Contact us</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Homepage')
+
+@section('content')
+    <h1>This is homepage</h1>
+@stop
